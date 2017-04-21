fddafa5dee3c6efc4d1b9993ba3ba232bedacb1c
hohguy <dfourney@gmail.com>
2017-04-20 21:14:02 -0400

    removeDuplicates solution

--- removeDuplicates/removeDuplicates.js
+++ removeDuplicates/removeDuplicates.js
@@ -4,5 +4,9 @@
  *
  */
 const removeDuplicates = (arr) => {
-	//code here...
+  const result = arr[0]
+  for (let i = 1; i < arr.length, i++) {
+    if (arr[i] !== arr[i-1]) result.push(arr[i]);
+  }
+  return result;
 };
