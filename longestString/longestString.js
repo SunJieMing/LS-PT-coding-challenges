c0f21ef51d9545ac49a6159cb69f6ec459b16678
hohguy <dfourney@gmail.com>
2017-04-24 21:14:01 -0400

    longestString solution

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -3,3 +3,12 @@
  * Return the longest string in the array.
  */
 
+ const longestString = (strArray) => {
+   let longestStr = '';
+   for (let i = (strArray.length - 1); i >= 0; i--) {
+     if (strArray[i].length > longestStr.length) {
+       longestStr = strArray[i];
+     }
+   }
+   return longestStr;
+ };
