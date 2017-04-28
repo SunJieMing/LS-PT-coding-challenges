f4670509ec4b2ddab346e32264b36f14d9e78500
hohguy <dfourney@gmail.com>
2017-04-27 21:27:10 -0400

    Solution

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -7,5 +7,19 @@
  */
 
 const selectionSort = (arr) => {
+  let indexMin;
 
-};
+  for (let i = 0; i < arr.length; i++) {
+    indexMin = i;
+
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[indexMin]) indexMin = j;
+    }
+    if (indexMin !== i) {
+      let temp = arr[i];
+      arr[i] = arr[indexMin];
+      arr[indexMin] = temp;
+    }
+  }
+  return arr;
+}
