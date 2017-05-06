0ce9c226d8ea96437ccd27d6138152dc4597924f
hohguy <dfourney@gmail.com>
2017-05-06 12:42:42 -0400

    Solution 2

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -4,17 +4,19 @@
  * Assume that each string will contain only spaces and letters.
  */
 
+ const isUpperCase = (str) => {
+   return str === str.toUpperCase();
+ }
+
  const reverseCase = (str) => {
    let result = '';
    for (let i = 0; i < str.length; i++) {
-     let char = str[i].charCodeAt();
-     if (char >= 65 && char <= 90) {
-       result += String.fromCharCode(char+32)
-     } else if (char >= 97 && char <= 122) {
-       result += String.fromCharCode(char-32)
-        } else {
-          result += str[i];
-        }
+     let char = str[i];
+     if (isUpperCase(char)) {
+       result += char.toLowerCase();
+     } else {
+       result += char.toUpperCase();
+     }
    }
    return result;
  }
