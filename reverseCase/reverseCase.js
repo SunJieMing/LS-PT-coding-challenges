bb19c60e64043fe19d62b58430641cdacd79f4bc
hohguy <dfourney@gmail.com>
2017-05-06 12:33:53 -0400

    Solution using character math

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -4,3 +4,18 @@
  * Assume that each string will contain only spaces and letters.
  */
 
+ const reverseCase = (str) => {
+   let result = '';
+   for (let i = 0; i < str.length; i++) {
+     let char = str[i].charCodeAt();
+     if (char >= 65 && char <= 90) {
+       result += String.fromCharCode(char+32)
+     } else if (char >= 97 && char <= 122) {
+       result += String.fromCharCode(char-32)
+        } else {
+          result += str[i];
+        }
+   }
+   return result;
+ }
+ 
