0902e15bd6edec0aea1e0e8543095f8a353aa638
hohguy <dfourney@gmail.com>
2017-05-08 22:34:14 -0400

    Solution

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -1,6 +1,6 @@
 /*
  *  * Find the first item that occurs an even number of times in an array.
- *   * Remember to handle multiple even-occurance items and return the first one. 
+ *   * Remember to handle multiple even-occurance items and return the first one.
  *    * Return null if there are no even-occurance items.
  *    */
 
@@ -10,6 +10,26 @@
  *    * console.log(onlyEven); //  4
  *    */
 
-const evenOccurence = (arr) => {
-  // Your code here.
-};
+ const isEven = (num) => {
+    return (num%2 === 0);
+  }
+
+ const evenOccurence = (arr) => {
+   if (arr.length === 0) return null;
+
+   let result = [];
+
+   for (let i = 0; i < arr.length; i++) {
+     let id = arr[i].toString();
+     if (result[id]) {
+       result[id] = result[id]+1;
+     } else {
+       result[id] = 1;
+     }
+   }
+
+   for (let i = 0; i < result.length; i++) {
+     if (isEven(result[i])) return i;
+   }
+   return null;
+ };
