3d1a6b837c91223602844ed0b1817b8ef51bb027
hohguy <dfourney@gmail.com>
2017-05-10 22:44:46 -0400

    Solution

--- breadthFirstSearch/breadthFirstSearch.js
+++ breadthFirstSearch/breadthFirstSearch.js
@@ -14,3 +14,28 @@
  * };
  * breadthFirstSearch(tree, 2);// will return true before it recursively searches `z`
  */
+
+
+ const isObject = (obj) => {
+   return obj === Object(obj);
+ }
+
+ const breadthFirstSearch = (treeToSearch, item) => {
+   let treeArray = Object.values(treeToSearch);
+   let treeList = [];
+
+   for (let i = 0; i < treeArray.length; i++) {
+     if (isObject(treeArray[i])) {
+       treeList.push(treeArray[i]);
+     } else {
+       if (treeArray[i] === item) return true;
+     }
+   }
+
+   let limit = treeList.length;
+
+   for (let j = 0; j < limit; j++) {
+     return breadthFirstSearch(treeList.pop(), item);
+   }
+   return false;
+ }
