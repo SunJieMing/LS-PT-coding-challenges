aab49cac69382e95fedba2827d109099f16f58f0
hohguy <dfourney@gmail.com>
2017-06-19 22:12:38 -0400

    Solution

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,30 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+const stringCompression = (str) => {
+  const result = [];
+  let letter = 0;
+  let count = 1;
+
+  result[letter] = str[0];
+  result[count] = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    if (result[letter] === str[i]) {
+      result[count] = result[count] + 1;
+    } else {
+      letter = letter + 2;
+      count = count + 2;
+      result[letter] = str[i];
+      result[count] = 1;
+    }
+  }
+  if (str.length <= result.length) {
+    return str;
+  } else {
+      let newString = result.toString();
+      newString = newString.replace (/,/g, "");
+      return newString;
+  }
+}
