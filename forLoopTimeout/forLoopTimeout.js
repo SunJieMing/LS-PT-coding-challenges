f0ec227f8928dcd856835b452c031dafc7e0be17
hohguy <dfourney@gmail.com>
2017-06-22 13:45:42 -0400

    Solution

--- forLoopTimeout/forLoopTimeout.js
+++ forLoopTimeout/forLoopTimeout.js
@@ -13,3 +13,21 @@ for (var i = 1; i <= 10; i++) {
 }
 
 //ES6 solution: Change "var" to "let".
+for (let i = 1; i <= 10; i++) {
+	setTimeout(function() {
+		// From looking at the code you would assume it would print 1 - 10
+		// It doesn't.  Why?  How can you make it print 1 - 10.
+		console.log(i);
+	}, 0);
+}
+
+//ES5 solution: IFFE.
+for (var i = 1; i <= 10; i++) {
+	(function (i) {
+	setTimeout(function() {
+		// From looking at the code you would assume it would print 1 - 10
+		// It doesn't.  Why?  How can you make it print 1 - 10.
+		console.log(i);
+	}, 0);
+})(i);
+}
